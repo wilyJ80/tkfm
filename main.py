a571a3f3ef99b6882db0c4175eaf0e8da6346616
wilyJ80 <abjurandam@gmail.com>
2024-04-08 21:35:10 -0300

    Window works

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import tkinter as tk
+from tkinter import ttk
+from tree import Node
 
 
 class GUI:
-    def __init__(self):
+    def __init__(self, root):
         window = tk.Tk()
+        window.title("tkfm")
+
+        treeview = ttk.Treeview()
+        treeview.insert("", tk.END, text=root.data)
+
+        treeview.pack()
         window.mainloop()
 
 
 if __name__ == "__main__":
-    GUI()
+    root = Node(4)
+    print(root.data)
+
+    GUI(root)
