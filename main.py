7fdbe015e31f606c9b66655b08069dd04e5c9696
Victor Hugo <abjurandam@gmail.com>
2024-04-14 23:06:52 -0300

    Root Node operations

--- main.py
+++ main.py
@@ -26,44 +26,42 @@ class GUI:
         self.child_button.pack()
 
         self.treeview.bind("<Button-1>", self.on_click)
+        self.selected_item = None
 
     def add_node(self, parent_item, node):
         item = self.treeview.insert(parent_item, tk.END, text=node.nome)
-        # if node.left:
-        #     self.add_node(item, node.left)
-        # if node.right:
-        #     self.add_node(item, node.right)
+        return item
 
     def add_root_node(self):
         content = simpledialog.askstring(
             "Add Root Node", "Enter the content for the new root node:")
-        if content is not None:
+        if content:
             new_node = Node_diretorio(content)
             self.add_node("", new_node)
 
-    def add_child_node(self, item):
-        content = simpledialog.askstring(
-            "Add Child Node", "Enter the content for the new child node:")
-        if content is not None:
-            new_node = Node_diretorio(content)
-            self.add_node(item, new_node)
+    def add_child_node(self):
+        if self.selected_item:
+            content = simpledialog.askstring(
+                "Add Child Node", "Enter the content for the new child node:")
+            if content:
+                new_node = Node_diretorio(content)
+                self.add_node(self.selected_item, new_node)
 
     def on_click(self, event):
-        item = self.treeview.focus()
-        if item:
-            # self.add_child_button = tk.Button(
-            #     self.window, text="Add Child Node", command=lambda: self.add_child_node(item))
-            # self.add_child_button.place(x=event.x_root, y=event.y_root)
-
+        self.selected_item = self.treeview.focus()
+        if self.selected_item:
             self.remove_button.config(state=tk.NORMAL)
             self.child_button.config(state=tk.NORMAL)
-
-    def remove_node(self):
-        selected_item = self.treeview.focus()
-        if selected_item:
-            self.treeview.delete(selected_item)
+        else:
             self.remove_button.config(state=tk.DISABLED)
             self.child_button.config(state=tk.DISABLED)
+
+    def remove_node(self):
+        if self.selected_item:
+            self.treeview.delete(self.selected_item)
+            self.remove_button.config(state=tk.DISABLED)
+            self.child_button.config(state=tk.DISABLED)
+            self.selected_item = None
 
     def run(self):
         self.treeview.pack()
