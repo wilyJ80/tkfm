3ebe7d4ddb3c347afebaa09b049e0c1941768d02
Bruno Rodrigues <bruno.rodriguesc9@gmail.com>
2024-04-15 22:51:40 -0300

    correção cd .., chamada da funçao, cd e mkdir, implementação do touch. obs: touch ainda com error na extensao

--- tree.py
+++ tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node_diretorio:
     def __init__(self, nome):
         self.nome = nome
@@ -28,7 +30,7 @@ class sistema_arquivo:
         else:
             encontrado = False
             for subdiretorio in self.atual.sub_diretorios:
-                if subdiretorio.nome == nome:
+                if subdiretorio.nome.lower() == nome.lower():
                     self.historico.append(self.atual)
                     self.atual = subdiretorio
                     encontrado = True
@@ -36,9 +38,8 @@ class sistema_arquivo:
             if not encontrado:
                 print('diretório não encontrado')
 
-    def mkdir(self):
+    def mkdir(self,nome_pasta):
         caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?']
-        nome_pasta = input('Digite o nome da pasta que voce quer criar: ')
         if any(caracter in nome_pasta for caracter in caracter_especial):
             print('Nome de pasta não pode conter caracteres especiais. Exemplo: ! @ # / : >')
         elif nome_pasta:
@@ -46,18 +47,25 @@ class sistema_arquivo:
             self.atual.sub_diretorios.append(novo_diretorio)
             print('Pasta {} criada com sucesso em {}'.format(nome_pasta, self.atual.nome))
 
-    def diretorio_pai(self, diretorio_atual, diretorio_alvo, pai=None):
-        if diretorio_atual == diretorio_alvo:
-            return pai
+    #problema criar com a extensão do arquivo
+    def touch(self,nome_arquivo):
+        caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '<', '>', '/', '?']
+        if any(caracter in nome_arquivo for caracter in caracter_especial):
+            print('Nome de pasta não pode conter caracteres especiais. Exemplo: ! @ # / : >')
+        else:
+            if nome_arquivo:
+                self.atual.arquivos.append(nome_arquivo)
+                print('Arquivo {} criado com sucesso' .format(nome_arquivo))
+            else:
+                pass
 
-        for subdiretorio in diretorio_atual.sub_diretorios:
-            resultado = self.diretorio_pai(subdiretorio, diretorio_alvo, diretorio_atual)
-            if resultado is not None:
-                return resultado
+    def mv(self,nome_arquivo,novo_nome_arquivo):
+        pass
 
-        return None
 
-# Criando diretorio
+
+
+# Criando pasta mas mudar para outro arquivo para organizar
 sistema = sistema_arquivo()
 docs = Node_diretorio('Documentos')
 img = Node_diretorio('Imagens')
@@ -67,22 +75,46 @@ sistema.raiz.sub_diretorios.extend([docs, img])
 docs.arquivos = ['doc1.txt', 'doc2.txt']
 img.arquivos = ['img.txt', 'img2.txt']
 
+
+
 if __name__ == "__main__":
     while True:
         comando = input('Diretorio atual {} Digite o comando: '.format(sistema.atual.nome))
-
         if comando == 'ls':
             sistema.ls()
+
         elif comando.startswith('cd'):
             partes = comando.split()
             if len(partes) == 2:
-                sistema.cd(partes[1])
+                sistema.cd(partes[1].capitalize())
             else:
                 print('Uso: cd <diretorio>')
-        elif comando == 'mkdir':
-            sistema.mkdir()
+
+        elif comando.startswith('mkdir'):
+            partes = comando.split()
+            if len(partes) == 2:
+                sistema.mkdir(partes[1].capitalize())
+            else:
+                print('Uso: mkdir <nome da pasta>')
+        
+        elif comando.startswith('touch'):
+            partes = comando.split()
+            if len(partes) == 2:
+                sistema.touch(partes[1].capitalize())
+            else:
+                print('Uso: touch <nome do arquivo>')
+
+        elif comando.startswith('mv'):
+            partes = comando.split()
+            if len(partes) == 3:
+                sistema.mv(partes[1].capitalize(), partes[2].capitalize())
+                pass
+            else:
+                pass
+
         elif comando == 'exit':
             print('Saindo do sistema')
             break
+
         else:
             pass
