b367a8b7ff6d21ae22c0a9569d18abb36d92022f
wilyJ80 <abjurandam@gmail.com>
2024-04-23 20:23:47 -0300

    Add update method for GUI class

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from tree import Node_diretorio, sistema_arquivo
 
 
 class GUI:
-    def __init__(self):
+    def __init__(self, sistema_arquivo):
         self.window = tk.Tk()
         self.window.title("Visualizer")
         self.window.geometry('250x250')
@@ -16,7 +16,16 @@ class GUI:
         self.treeview.pack()
         self.treeview.mainloop()
 
+    def update(self, sistema_arquivo):
+        self.treeview.delete(*self.treeview.get_children())
+        node = sistema_arquivo.atual
+        self.treeview.heading('#0', text=node.nome)
+        for subdir in node.sub_diretorios:
+            self.treeview.insert("", tk.END, text="/" + subdir.nome)
+        for arquivo in node.arquivos:
+            self.treeview.insert("", tk.END, text=arquivo)
+
 
 if __name__ == "__main__":
-    gui = GUI()
+    gui = GUI(sistema_arquivo)
     gui.run()
