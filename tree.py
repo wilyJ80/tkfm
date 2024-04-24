9e50c14f320b0bdaa80e23da08d760c48af51610
Bruno Rodrigues <bruno.rodriguesc9@gmail.com>
2024-04-23 21:12:40 -0300

    add mkdir nome da pasta e diretorio, comentarios e pequenas correções

--- tree.py
+++ tree.py
@@ -14,6 +14,9 @@ class sistema_arquivo:
         self.historico = []
 
     def ls(self):
+        """
+        função para listar os arquivos e diretorios daquela parte
+        """
         print('Conteúdo de', self.atual.nome)
         for subdiretorio in self.atual.sub_diretorios:
             print('{}/ (diretorio)'.format(subdiretorio.nome))
@@ -21,6 +24,9 @@ class sistema_arquivo:
             print('arquivo: {}'.format(arquivo))
 
     def cd(self, nome):
+        """
+        função para voltar um diretorio ou voltar 2
+        """
         if nome == '..':
             self.mover_diretorio(1)
         elif nome == '../..':
@@ -37,6 +43,9 @@ class sistema_arquivo:
                 print('diretório não encontrado')
 
     def mover_diretorio(self, nivel):
+        """
+        função usada para voltar os diretorios
+        """
         if nivel == 0:
             return
         if nivel == 1:
@@ -51,19 +60,45 @@ class sistema_arquivo:
             else:
                 print("Já está no diretório raiz")
 
-    def mkdir(self, nome_pasta):
-        caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*',
-                             '(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?']
-        if any(caracter in nome_pasta for caracter in caracter_especial):
-            print(
-                'Nome de pasta não pode conter caracteres especiais. Exemplo: ! @ # / : >')
-        elif nome_pasta:
-            novo_diretorio = Node_diretorio(nome_pasta)
-            self.atual.sub_diretorios.append(novo_diretorio)
-            print('Pasta {} criada com sucesso em {}'.format(
-                nome_pasta, self.atual.nome))
+    def mkdir(self, nome_pasta, dir=None, nivel=2):
+        """
+        função para criar pasta no diretorio atual ou especificando um caminho
+        """
+        if nivel == 2:
+            caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*','(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?']
+            if any(caracter in nome_pasta for caracter in caracter_especial):
+                print('Nome de pasta não pode conter caracteres especiais. Exemplo: ! @ # / : >')
+            elif nome_pasta:
+                novo_diretorio = Node_diretorio(nome_pasta)
+                self.atual.sub_diretorios.append(novo_diretorio)
+                print('Pasta {} criada com sucesso em {}'.format(nome_pasta, self.atual.nome))
+        elif nivel == 3:
+            caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*','(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?']
+            if any(caracter in nome_pasta for caracter in caracter_especial):
+                print('Nome de pasta não pode conter caracteres especiais. Exemplo: ! @ # / : >')
+            elif dir:
+                partes = dir.split('/')
+                diretorio_atual = self.raiz
+                for parte in partes:
+                    encontrado = False
+                    for subdiretorio in diretorio_atual.sub_diretorios:
+                        if subdiretorio.nome.lower() == parte.lower():
+                            diretorio_atual = subdiretorio
+                            encontrado = True
+                            break
+                    if not encontrado:
+                        print('Diretório {} não encontrado'.format(parte))
+                        return
+                novo_diretorio = Node_diretorio(nome_pasta)
+                diretorio_atual.sub_diretorios.append(novo_diretorio)
+                print('Pasta {} criada com sucesso em {}'.format(nome_pasta, diretorio_atual.nome))
+            else:
+                print('Caminho não especificado')
 
     def touch(self, nome_arquivo):
+        """
+        função para criar arquivo
+        """
         caracter_especial = ['!', '@', '#', '$', '%', '^', '&', '*',
                              '(', ')', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', ',', '<', '>', '/', '?']
         if any(caracter in nome_arquivo for caracter in caracter_especial):
@@ -77,6 +112,9 @@ class sistema_arquivo:
                 pass
 
     def mv(self, nome_origem, novo_nome):
+        """
+        função para renomear arquivo
+        """
         encontrado = False
         for i, arquivo in enumerate(self.atual.arquivos):
             if arquivo == nome_origem:
@@ -89,6 +127,9 @@ class sistema_arquivo:
             print('Arquivo não encontrado: {}'.format(nome_origem))
 
     def rm(self, nome_arquivo):
+        """
+        função para deletar arquivo
+        """
         encontrado = False
         for arquivo in self.atual.arquivos:
             if arquivo == nome_arquivo:
@@ -99,16 +140,19 @@ class sistema_arquivo:
         if not encontrado:
             print('Arquivo não encontrado: {}'.format(nome_arquivo))
 
-    def print_arvore(self, node, prefix=""):
+    def print_tree(self, node, prefix=""):
+        """
+        função para printar toda arvore
+        """
         for subdir in node.sub_diretorios:
             print(prefix + "|-- " + subdir.nome + "/")
-            self.print_arvore(subdir, prefix + "|   ")
+            self.print_tree(subdir, prefix + "|   ")
         for arquivo in node.arquivos:
             print(prefix + "|-- " + arquivo)
 
-    def arvore(self):
+    def tree(self):
         print(self.atual.nome, '/')
-        self.print_arvore(self.atual)
+        self.print_tree(self.atual)
 
 
 # Criando pasta mas mudar para outro arquivo para organizar
@@ -139,9 +183,11 @@ if __name__ == "__main__":
         elif comando.startswith('mkdir'):
             partes = comando.split()
             if len(partes) == 2:
-                sistema.mkdir(partes[1].capitalize())
+                sistema.mkdir(partes[1].capitalize(),None,2)
+            elif len(partes) == 3:
+                sistema.mkdir(partes[1].capitalize(),partes[2].capitalize(),3)
             else:
-                print('Uso: mkdir <nome da pasta>')
+                print('Uso: mkdir <nome da pasta> ou mkdir <nome da pasta> <nome do diretorio que quer inserir>')
 
         elif comando.startswith('touch'):
             partes = comando.split()
@@ -157,17 +203,17 @@ if __name__ == "__main__":
                 pass
             else:
                 print(
-                    'Use mv <nome do arquivo que quer alterar>  <nome do novo arquivo>')
+                    'Uso: mv <nome do arquivo que quer alterar>  <nome do novo arquivo>')
 
         elif comando.startswith('rm'):
             partes = comando.split()
             if len(partes) == 2:
                 sistema.rm(partes[1].capitalize())
             else:
-                print('Use rm <nome do arquivo que quer excluir>')
+                print('Uso: rm <nome do arquivo que quer excluir>')
 
         elif comando == 'tree':
-            sistema.arvore()
+            sistema.tree()
 
         elif comando == 'help':
             print("Comandos:")
